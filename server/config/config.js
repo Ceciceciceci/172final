c87c497fc6d856fdae7bca675819a6d90ea9c0f5
Ceci Tran <ceci@Cecis-MacBook-Pro.local>
2016-12-16 08:50:40 -0800

    push 1

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	// disbable logging for testing
+logging: false,
+  db: {
+        // the database url to connect!                                                                                                                                              
+        url: 'mongodb://ec2-35-162-28-111.us-west-2.compute.amazonaws.com:27017/finalsdb'
+    }
+}
